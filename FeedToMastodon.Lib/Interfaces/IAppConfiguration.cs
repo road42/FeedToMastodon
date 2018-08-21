44de13e912a84be3385ae2d3d1240b8670f1670e
Christoph Jahn <christoph.jahn@scherdel.de>
2018-08-21 13:48:16 +0200

    Lib: Created service and configuration interfaces

---
+++ FeedToMastodon.Lib/Interfaces/IAppConfiguration.cs
@@ -0,0 +1,17 @@
+/*
+    Copyright (c) 2018 Christoph Jahn.
+
+    This file is licensed to you under the MIT license.
+    See the LICENSE.txt file in the project root for more information.
+*/
+
+namespace FeedToMastodon.Lib.Interfaces
+{
+    /*
+        Provides the application configration and some methods to write it
+    */
+    public interface IAppConfiguration
+    {
+
+    }
+}
---
+++ FeedToMastodon.Lib/Interfaces/IInstanceService.cs
@@ -0,0 +1,18 @@
+/*
+    Copyright (c) 2018 Christoph Jahn.
+
+    This file is licensed to you under the MIT license.
+    See the LICENSE.txt file in the project root for more information.
+*/
+
+namespace FeedToMastodon.Lib.Interfaces
+{
+    /*
+        Provides the connection to a mastodon-instance and
+        methods to work with it.
+     */
+    public interface IInstanceService
+    {
+
+    }
+}
