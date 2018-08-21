b35d66b9b9e2f2f8639d32533291739959f4db67
Christoph Jahn <christoph.jahn@scherdel.de>
2018-08-21 14:53:03 +0200

    Cli: Added documentaion to RegisterApplication.cs Cli: Added service injection to RegisterApplication.cs

--- FeedToMastodon.Cli/Commands/RegisterApplication.cs
+++ FeedToMastodon.Cli/Commands/RegisterApplication.cs
@@ -7,35 +7,62 @@
 
 using System;
 using System.ComponentModel.DataAnnotations;
-
+using FeedToMastodon.Lib.Interfaces;
 using McMaster.Extensions.CommandLineUtils;
 
 namespace FeedToMastodon.Cli.Commands
 {
+    /*
+        This command is needed first. You have to register the
+        application with the instance.
+
+        If you want to run multiple versions of the application you
+        have to choose an individual name for each one.
+    */
     [Command(
         "register",
         Name = Program.PROGRAMNAME,
         FullName = Program.PROGRAMFULLNAME,
         Description = "Registers the application with a mastodon instance",
-        ExtendedHelpText = Program.EXTENDEDHELPTEXT
+        ExtendedHelpText = Program.EXTENDEDHELPTEXT,
+        ThrowOnUnexpectedArgument = false
     )]
     public class RegisterApplication
     {
+#region "Options"
 
+        // The instanceName: we create the Uri with it.
         [Required(ErrorMessage = "You must specify an instance name")]
         [Option("--instance", Description = "The name of the mastodon instance (without https://)")]
         private string instanceName { get; set; }
 
+        // Choose an individual name for the application if you want to
+        // use multiple instances.
         [Option("--appName", Description = "The name used to register this application (optional)")]
         private string appName { get; set; } = Program.APPNAME;
 
-
+        // You may set this in mastodon. Not really needed here.
         [Option("--appSite", Description = "The website of the application (optional)")]
         private string appSite { get; set; } = Program.APPSITE;
 
-        private void OnExecute(IConsole console)
+#endregion
+
+        private IAppConfiguration configuration;
+        private IInstanceService instanceService;
+        private IConsole console;
+
+        // The required services are injected. Registration is in Program.cs
+        public RegisterApplication(IAppConfiguration configuration, IInstanceService instanceService, IConsole console)
         {
-            Console.WriteLine($"i: {instanceName} n: {appName} s: {appSite}");
+            this.configuration = configuration;
+            this.instanceService = instanceService;
+            this.console = console;
+        }
+
+        // If the Options are set and the command should run.
+        private void OnExecute()
+        {
+            console.WriteLine($"i: {instanceName} n: {appName} s: {appSite}");
             /*
                     TODO: Put to library
                     TODO: Add config file option to write data to
