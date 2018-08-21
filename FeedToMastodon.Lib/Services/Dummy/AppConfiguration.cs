eb13a5cc811cf2c499e8846fdc2b00608d599e9e
Christoph Jahn <christoph.jahn@scherdel.de>
2018-08-21 13:48:41 +0200

    Lib: Created dummy implementation of interfaces for tdd

---
+++ FeedToMastodon.Lib/Services/Dummy/AppConfiguration.cs
@@ -0,0 +1,18 @@
+/*
+    Copyright (c) 2018 Christoph Jahn.
+
+    This file is licensed to you under the MIT license.
+    See the LICENSE.txt file in the project root for more information.
+*/
+
+namespace FeedToMastodon.Lib.Services.Dummy
+{
+    /*
+        A dummy-implementation of the AppConfiguration just does everything
+        InMemory for tests and development.
+    */
+    public class AppConfiguration : Interfaces.IAppConfiguration
+    {
+
+    }
+}
