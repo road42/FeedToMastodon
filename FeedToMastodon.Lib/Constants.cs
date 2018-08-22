487d6b76deb15f3c3073ae67ae6972d6b75d2a9b
Christoph Jahn <cj@road42.de>
2018-08-22 11:50:32 +0200

    Lib: Introduced Constants from Program.cs

---
+++ FeedToMastodon.Lib/Constants.cs
@@ -0,0 +1,22 @@
+/*
+    Copyright (c) 2018 Christoph Jahn.
+
+    This file is licensed to you under the MIT license.
+    See the LICENSE file in the project root for more information.
+*/
+
+namespace FeedToMastodon.Lib
+{
+    public static class Constants
+    {
+        public const string PROGRAMNAME = "FeedToMastodon";
+        public const string PROGRAMFULLNAME = "FeedToMastodon (Version: 0.0.1)";
+        public const string EXTENDEDHELPTEXT = "\nSee more information about the software at: https://github.com/road42/FeedToMastodon \n(c) 2018 Christoph Jahn - Licensed under MIT license (See LICENSE)\n";
+
+        public const string APPNAME = "FeedToMastodon";
+        public const string APPSITE = "https://github.com/road42/FeedToMastodon";
+
+        public const string DEFAULT_CONFIGCONNECTIONSTRING = "feedtomastodon.cfg.json";
+        public const string ENVIRONMENT_CONFIGCONNECTIONSTRING_NAME = "FEEDTOMASTODON_CFG";
+    }
+}
