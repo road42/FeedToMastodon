181f7e551ef0c4b5a10304d07622377aefab1916
Christoph Jahn <cj@road42.de>
2018-08-23 09:13:07 +0200

    Lib: Massive feedService rework Lib: feedService - removed duplicated text Lib: feesService - creates id in feed, when none found

--- FeedToMastodon.Lib/Services/FeedService.cs
+++ FeedToMastodon.Lib/Services/FeedService.cs
@@ -5,7 +5,11 @@
     See the LICENSE.txt file in the project root for more information.
 */
 
+using System;
+using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
 using System.Threading.Tasks;
 using System.Xml;
 using FeedToMastodon.Lib.Interfaces;
@@ -44,15 +48,10 @@ namespace FeedToMastodon.Lib.Services
                 // Other types prepared :-)
                 switch (feed.Type)
                 {
-                    // RSS Feed
+                    // RSS2.0 or Atom Feed
                     case FeedType.RSS:
-                        await HandleRssFeed(feed, populateCacheOnly);
-                        break;
-
-                    // Atom Feed
                     case FeedType.Atom:
-                        await HandleAtomFeed(feed, populateCacheOnly);
-                        break;
+                        return await HandleXmlFeed(feed, populateCacheOnly);
 
                     // Anything else is ignored
                     default:
@@ -63,61 +62,52 @@ namespace FeedToMastodon.Lib.Services
             return true;
         }
 
-        private async Task<bool> HandleRssFeed(Feed feed, bool populateCacheOnly = false)
+        //TODO: Combine Rss and Atmom feed handling. (Too much copies)
+        private async Task<bool> HandleXmlFeed(Feed feed, bool populateCacheOnly = false)
         {
-            var feedTemplate = feed?.Toot?.Template;
-            var feedVisbility = feed?.Toot?.Visibiliy;
-
             try
             {
                 using (XmlReader xmlReader = XmlReader.Create(feed.ConnectionString, new XmlReaderSettings() { Async = true }))
                 {
-                    var reader = new RssFeedReader(xmlReader);
+                    ISyndicationFeedReader reader = null;
+
+                    switch (feed.Type)
+                    {
+                        case FeedType.RSS:
+                            reader = new RssFeedReader(xmlReader);
+                            break;
+                        case FeedType.Atom:
+                            reader = new AtomFeedReader(xmlReader);
+                            break;
+                        default:
+                            return false;
+                    }
 
                     while (await reader.Read())
                     {
                         if (reader.ElementType == SyndicationElementType.Item)
                         {
-                            ISyndicationItem entry = await reader.ReadItem();
+                            FeedEntry feedEntry = null;
 
-                            if (!await cache.IsCached(entry.Id))
+                            switch (feed.Type)
                             {
-                                var feedEntry = new FeedEntry()
-                                {
-                                    FeedTitle = feed.Name,
-                                    Id = entry.Id,
-                                    Title = entry.Title,
-                                    Summary = string.Empty,
-                                    Description = entry.Description,
-                                    LastUpdated = entry.LastUpdated,
-                                    Published = entry.Published,
-                                    Link = entry?.Links?.First(r => r.RelationshipType == "alternate")?.Uri.ToString()
-                                };
-
-                                if (entry.Categories != null)
-                                {
-                                    foreach (var category in entry.Categories)
-                                    {
-                                        feedEntry.Tags.Add(category.Name);
-                                    }
-                                }
-
-                                if (!populateCacheOnly)
-                                {
-                                    var tootResult = await instance.TootFeedEntry(feedEntry, feedTemplate, feedVisbility);
-
-                                    if (!tootResult)
-                                        return false;
-                                }
-
-                                // Cache the entry
-                                await cache.Cache(feed.Name, entry.Id, entry.Published.DateTime);
+                                case FeedType.RSS:
+                                    ISyndicationItem rssEntry = await (reader as RssFeedReader).ReadItem();
+                                    feedEntry = CreateFeedEntry(rssEntry, feed);
+                                    break;
+                                case FeedType.Atom:
+                                    IAtomEntry atomEntry = await (reader as AtomFeedReader).ReadEntry();
+                                    feedEntry = CreateFeedEntry(atomEntry, feed);
+                                    break;
+                                default:
+                                    continue;
                             }
+
+                            if (!(await cache.IsCached(feedEntry.Id)))
+                                return await TootTheFeedEntry(feedEntry, populateCacheOnly, feed);
                         }
                     }
                 }
-
-                return true;
             }
             catch
             {
@@ -127,70 +117,113 @@ namespace FeedToMastodon.Lib.Services
             return false;
         }
 
-        private async Task<bool> HandleAtomFeed(Feed feed, bool populateCacheOnly = false)
+        private async Task<bool> TootTheFeedEntry(FeedEntry entry, bool populateCacheOnly, Feed feed)
         {
-            var feedTemplate = feed?.Toot?.Template;
-            var feedVisbility = feed?.Toot?.Visibiliy;
+            // Toot if wanted
+            if (!populateCacheOnly && !(await instance.TootFeedEntry(entry, feed?.Toot?.Template, feed?.Toot?.Visibiliy)))
+                return false;
 
-            try
+            // Cache the entry
+            await cache.Cache(feed.Name, entry.Id, entry.Published.DateTime);
+
+            return true;
+        }
+
+        private FeedEntry CreateFeedEntry(IAtomEntry entry, Feed feed)
+        {
+            var link = GetLinkFromFeedEntry(entry.Links);
+
+            var feedEntry = new FeedEntry()
             {
-                using (XmlReader xmlReader = XmlReader.Create(feed.ConnectionString, new XmlReaderSettings() { Async = true }))
+                FeedTitle = feed.Name,
+                Id = entry.Id,
+                Title = entry.Title,
+                Summary = entry.Summary,
+                Description = entry.Description,
+                LastUpdated = entry.LastUpdated,
+                Published = entry.Published,
+                Link = link
+            };
+
+            if (feedEntry.Id == null)
+                feedEntry.Id = CreateIdFromFeedEntry(feedEntry);
+
+            //TODO: This could be better
+            if (entry.Categories != null)
+            {
+                foreach (var category in entry.Categories)
                 {
-                    var reader = new AtomFeedReader(xmlReader);
-
-                    while (await reader.Read())
-                    {
-                        if (reader.ElementType == SyndicationElementType.Item)
-                        {
-                            IAtomEntry entry = await reader.ReadEntry();
-
-                            if (!await cache.IsCached(entry.Id))
-                            {
-
-                                var feedEntry = new FeedEntry()
-                                {
-                                    FeedTitle = feed.Name,
-                                    Id = entry.Id,
-                                    Title = entry.Title,
-                                    Summary = entry.Summary,
-                                    Description = entry.Description,
-                                    LastUpdated = entry.LastUpdated,
-                                    Published = entry.Published,
-                                    Link = entry?.Links?.First()?.Uri.ToString()
-                                };
-
-                                if (entry.Categories != null)
-                                {
-                                    foreach (var category in entry.Categories)
-                                    {
-                                        feedEntry.Tags.Add(category.Name);
-                                    }
-                                }
-
-
-                                if (!populateCacheOnly)
-                                {
-                                    var tootResult = await instance.TootFeedEntry(feedEntry, feedTemplate, feedVisbility);
-
-                                    if (!tootResult)
-                                        return false;
-                                }
-
-                                // Cache the entry
-                                await cache.Cache(feed.Name, entry.Id, entry.Published.DateTime);
-                            }
-                        }
-                    }
+                    feedEntry.Tags.Add(category.Name);
                 }
-
-                return true;
             }
-            catch
+
+            return feedEntry;
+        }
+
+        private FeedEntry CreateFeedEntry(ISyndicationItem entry, Feed feed)
+        {
+            var link = GetLinkFromFeedEntry(entry.Links);
+
+            var feedEntry = new FeedEntry()
             {
+                FeedTitle = feed.Name,
+                Id = entry.Id,
+                Title = entry.Title,
+                Summary = null,
+                Description = entry.Description,
+                LastUpdated = entry.LastUpdated,
+                Published = entry.Published,
+                Link = link
+            };
 
+            if (feedEntry.Id == null)
+                feedEntry.Id = CreateIdFromFeedEntry(feedEntry);
+
+            //TODO: This could be better
+            if (entry.Categories != null)
+            {
+                foreach (var category in entry.Categories)
+                {
+                    feedEntry.Tags.Add(category.Name);
+                }
             }
 
-            return false;
+            return feedEntry;
+        }
+
+        private string GetLinkFromFeedEntry(IEnumerable<ISyndicationLink> links)
+        {
+            var link = links?.First(r => r.RelationshipType == "alternate")?.Uri.ToString();
+
+            if (string.IsNullOrWhiteSpace(link))
+                link = links?.First()?.Uri.ToString();
+
+            if (string.IsNullOrWhiteSpace(link))
+                return null;
+
+            return link;
+        }
+
+        private string CreateIdFromFeedEntry(FeedEntry entry)
+        {
+            // Check if there is an id
+            if (entry.Id != null)
+                return entry.Id;
+
+            // Create a new Id
+            var key = $"{entry.FeedTitle}-{entry.Title}-{entry.Link}";
+
+            var mySHA256 = SHA256Managed.Create();
+            var hash = mySHA256.ComputeHash(Encoding.UTF8.GetBytes(key));
+
+            var id = new System.Text.StringBuilder();
+
+            foreach (byte theByte in hash)
+            {
+                id.Append(theByte.ToString("x2"));
+            }
+
+            return id.ToString();
         }
     }
 }
