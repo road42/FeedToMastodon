42abeef39e73d7c142ec8d314674ce720b2a6122
Christoph Jahn <cj@road42.de>
2019-02-15 16:50:17 +0100

    Solution: New version 0.2.2

--- Scripts/publish.sh
+++ Scripts/publish.sh
@@ -12,15 +12,15 @@ dotnet publish -c Release -r linux-x64
 dotnet publish -c Release -r win-x64
 dotnet publish -c Release -r osx-x64
 
-mv ./FeedToMastodon.Cli/bin/Release/netcoreapp2.1/linux-x64/publish ./FeedToMastodon-linux-x64-0.2.1
-mv ./FeedToMastodon.Cli/bin/Release/netcoreapp2.1/win-x64/publish ./FeedToMastodon-win-x64-0.2.1
-mv ./FeedToMastodon.Cli/bin/Release/netcoreapp2.1/osx-x64/publish ./FeedToMastodon-osx-x64-0.2.1
+mv ./FeedToMastodon.Cli/bin/Release/netcoreapp2.2/linux-x64/publish ./FeedToMastodon-linux-x64-0.2.2
+mv ./FeedToMastodon.Cli/bin/Release/netcoreapp2.2/win-x64/publish ./FeedToMastodon-win-x64-0.2.2
+mv ./FeedToMastodon.Cli/bin/Release/netcoreapp2.2/osx-x64/publish ./FeedToMastodon-osx-x64-0.2.2
 
-tar czvf FeedToMastodon-linux-x64-0.2.1.tar.gz FeedToMastodon-linux-x64-0.2.1/
-zip -r FeedToMastodon-win-x64-0.2.1.zip FeedToMastodon-win-x64-0.2.1/
-zip -r FeedToMastodon-osx-x64-0.2.1.zip FeedToMastodon-osx-x64-0.2.1/
+tar czvf FeedToMastodon-linux-x64-0.2.2.tar.gz FeedToMastodon-linux-x64-0.2.2/
+zip -r FeedToMastodon-win-x64-0.2.2.zip FeedToMastodon-win-x64-0.2.2/
+zip -r FeedToMastodon-osx-x64-0.2.2.zip FeedToMastodon-osx-x64-0.2.2/
 
 
-rm -rf ./FeedToMastodon-linux-x64-*
-rm -rf ./FeedToMastodon-win-x64-*
-rm -rf ./FeedToMastodon-osx-x64-*
+rm -rf ./FeedToMastodon-linux-x64-0.2.2
+rm -rf ./FeedToMastodon-win-x64-0.2.2
+rm -rf ./FeedToMastodon-osx-x64-0.2.2
